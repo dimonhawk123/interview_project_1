9ce6566a5b4c4b791bbaf01ad6cdd99c1ead689a
DmitriiRyzhkov <ryzhkov.eecs@gmail.com>
2020-01-14 23:51:16 +0500

    gulp file have created

--- builder/gulpfile.js
+++ builder/gulpfile.js
@@ -1,12 +1,27 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('styles', function() {
+    return gulp.src('../src/BEM/common.blocks/*.scss')
+        .pipe(sass())
+        .pipe(concat('styles.css'))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(gulp.dest('../build/css'));
 
 });
 
 gulp.task('scripts', function() {
-    
+    return gulp.src('../src/BEM/common.blocks/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('../build/js'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('../src/BEM/common.blocks/*.scss', gulp.series('styles'));
+    gulp.watch('../src/BEM/common.blocks/*.js', gulp.series('scripts'));
 });
