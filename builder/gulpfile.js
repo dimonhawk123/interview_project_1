3cb3dfe9d8a4adb736aceeb364898e625cfee95b
DmitriiRyzhkov <ryzhkov.eecs@gmail.com>
2020-01-14 22:19:27 +0500

    test commit

---
+++ builder/gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+
+gulp.task('styles', function() {
+
+});
+
+gulp.task('scripts', function() {
+    
+});
