5741a2b76982f2bff283d3a621906e102edfb55b
DmitriiRyzhkov <ryzhkov.eecs@gmail.com>
2020-01-19 18:00:35 +0500

    slider has done

--- src/BEM/common.blocks/testJS.js
+++ src/BEM/common.blocks/testJS.js
@@ -1,3 +1,5 @@
+//-------------------------------------------------
+
 let arr = Array.from(document.querySelector('.about .block').children);
 let links = document.querySelectorAll('.about .menu__link');
 
@@ -22,6 +24,8 @@ function about(event) {
     })    
 }
 
+//-------------------------------------------------
+
 let arr_edu = Array.from(document.querySelector('.edu .block').children);
 let links_edu = document.querySelectorAll('.edu .menu__link');
 
@@ -45,3 +49,61 @@ function edu(event) {
         }        
     })    
 }
+
+//-------------------------------------------------
+
+let leftBtn = document.querySelector('.slider-btn__left');
+let rightBtn = document.querySelector('.slider-btn__right');
+let elemCount = document.querySelector('.slider__list').children.length;
+let slider = document.querySelector('.slider__list');
+let visibleElem = 4;
+let iter = 0;
+let interval;
+let countPath = 0;
+let width =  document.querySelector('.path_gray');
+
+leftBtn.onclick = function() {
+    clearInterval(interval);
+    countPath = 0;
+    iter--;
+    if (iter < 0) {
+        iter = elemCount - visibleElem;
+    }
+    slider.style.marginLeft = iter * -300 + 'px';
+    interval = setInterval(pathWidth, 40);
+}
+
+rightBtn.onclick = function() {
+    clearInterval(interval);
+    countPath = 0;
+    switcher();
+    interval = setInterval(pathWidth, 40);
+}
+
+function switcher() {
+    iter++;
+    if(iter > (elemCount - visibleElem)) {
+        iter = 0;
+    }
+    slider.style.marginLeft = iter * -300 + 'px';
+}
+
+// function pathWidth() {
+//     countPath++;
+//     if (countPath == 101) {
+//         countPath = 0;        
+//     }
+//     width.style.width = countPath + '%';
+// }
+// interval = setInterval(pathWidth, 50);
+
+function pathWidth() {
+    countPath++;
+    if (countPath == 151) {
+        countPath = 0;   
+        switcher();     
+    }
+    width.style.width = (1198/150) * countPath + 'px';
+}
+
+interval = setInterval(pathWidth, 40);
